69faf8d2c3e2fa3f8ec5942592624b4deea45550
Frida Gidlund <fridagidlund@gmail.com>
2019-04-04 16:27:29 +0200

    Added Constructor

--- PearlsDesign/Models/PearlGrid.cs
+++ PearlsDesign/Models/PearlGrid.cs
@@ -6,10 +6,35 @@ namespace PearlsDesign.Models
     internal class PearlGrid
     {
         private Color defaultColor = Colors.White;
-        public int PearlGridSize { get; set; }
-        public double PearlBeadSize { get; set; }
+
+        public List<Pearl> Pearls;
+        public int PearlGridSize { get; private set; }
+        public double PearlBeadSize { get; private set; }
         public Color DefaultColor { get => defaultColor; set => defaultColor = value; }
 
-        public List<Pearl> Pearls = new List<Pearl>();
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="size"></param>
+        /// <param name="pearlBeadSize"></param>
+        public PearlGrid(int size, double pearlBeadSize)
+        {
+            PearlGridSize = size;
+            PearlBeadSize = pearlBeadSize;
+        }
+
+        /// <summary>
+        /// Overwrite Pearls with new Pearl Objects
+        /// </summary>
+        public void GeneratePearlList()
+        {
+            var pearls = new List<Pearl>();
+            var times = PearlGridSize ^ 2;
+            for (int i = 0; i < times; i++)
+            {
+                pearls.Add(new Pearl(i, defaultColor));
+            }
+            Pearls = pearls;
+        }
     }
 }
